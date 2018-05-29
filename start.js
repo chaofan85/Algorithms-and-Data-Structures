96f34df3b5ecfec630217538f59532671ee1d77f
Chao Fan <fanchao05@gmail.com>
2018-05-28 23:53:05 -0400

    updated

--- start.js
+++ start.js
@@ -1,1 +1,3 @@
-console.log('hi, this is Chao. I want a job. And Darryl. Also a lot of money');
+console.log(
+  'hi, this is Chao. I want a job. And Darryl. Also a lot of money. I want an easy life.'
+);
