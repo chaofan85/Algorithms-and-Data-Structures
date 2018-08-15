712861f77ac2707cd087fdb14b2b4d7826088e1c
Chao Fan <fanchao05@gmail.com>
2018-08-15 17:51:42 -0400

    added BST implementation

---
+++ review/BST_081518.js
@@ -0,0 +1,51 @@
+// Implementing Binary Search Tree
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, node = this.root) {
+    if (!this.root) {
+      this.root = new Node(val);
+      return;
+    }
+
+    if (val < node.val) {
+      if (node.left) {
+        this.insert(val, node.left);
+      } else {
+        node.left = new Node(val);
+      }
+    } else {
+      if (node.right) {
+        this.insert(val, node.right);
+      } else {
+        node.right = new Node(val);
+      }
+    }
+  }
+
+  inorder(node = this.root) {
+    if (!node) return;
+    this.inorder(node.left);
+    console.log(node.val);
+    this.inorder(node.right);
+  }
+}
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(16);
+tree.insert(13);
+tree.inorder();
---
+++ review/scope.js
@@ -0,0 +1,4 @@
+// only var declarations are hoisted, not assignment
+
+console.log(bootcamp);
+var bootcamp = 'hi';
