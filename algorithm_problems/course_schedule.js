f807ba3a3f0cff1ba148f2f25ce94f0907878712
Chao Fan <fanchao05@gmail.com>
2018-09-06 00:08:10 -0400

    added coure schedule problem

---
+++ algorithm_problems/course_schedule.js
@@ -0,0 +1,41 @@
+var canFinish = function(numCourses, prerequisites) {
+  console.log(getPreList(prerequisites));
+  let parents = getPreList(prerequisites);
+  let visited = new Set([]);
+  let len = Object.keys(parents).length;
+
+  while (visited.size < len) {
+    let curr = visited.size;
+    for (let node in parents) {
+      if (!parents[node].length) {
+        visited.add(node);
+      } else {
+        if (parents[node].every(parent => visited.has(parent.toString()))) {
+          visited.add(node);
+        }
+      }
+    }
+    if (curr === visited.size) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function getPreList(prereqs) {
+  let preList = { "0": [] };
+  for (let i = 0; i < prereqs.length; i++) {
+    if (!preList[prereqs[i][1]]) {
+      preList[prereqs[i][1]] = [];
+    }
+    if (!preList[prereqs[i][0]]) {
+      preList[prereqs[i][0]] = [prereqs[i][1]];
+    } else {
+      preList[prereqs[i][0]].push(prereqs[i][1]);
+    }
+  }
+
+  return preList;
+}
+
+console.log(canFinish(2, [[0, 1]]));
