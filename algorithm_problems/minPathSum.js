314a7f8fd85af3938f52b9bdaf5403c54958f327
Chao Fan <fanchao05@gmail.com>
2018-09-11 00:46:05 -0400

    updated minPathSum

--- algorithm_problems/minPathSum.js
+++ algorithm_problems/minPathSum.js
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/minimum-path-sum/description/
 
-function minPathSum(grid, row = 0, col = 0) {
-  if (row === grid.length || col === grid[0].length) return;
+function minPathSum(grid, row = 0, col = 0, memo = {}) {
+  let key = `${row}-${col}`;
 
-  console.log(row, col);
+  if (row >= grid.length || col >= grid[0].length) return Infinity;
+  if (row === grid.length - 1 && col === grid[0].length - 1)
+    return grid[row][col];
 
-  minPathSum(grid, row, col + 1);
-  minPathSum(grid, row + 1, col);
+  if (key in memo) {
+    return memo[key];
+  }
+
+  let result =
+    grid[row][col] +
+    Math.min(
+      minPathSum(grid, row + 1, col, memo),
+      minPathSum(grid, row, col + 1, memo)
+    );
+
+  memo[key] = result;
+  return memo[key];
 }
+
+console.log(minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]));
