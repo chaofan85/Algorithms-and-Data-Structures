44f814d33d246aae280be3241e14e5e7d4d8ec11
Chao Fan <fanchao05@gmail.com>
2018-09-19 14:33:00 -0400

    added partition list problem

--- algorithm_problems/cracking_interview/Linked_List/partitionList.js
+++ algorithm_problems/cracking_interview/Linked_List/partitionList.js
@@ -5,13 +5,13 @@ class Node {
   }
 }
 
-var a = new Node(1);
-var b = new Node(2);
+var a = new Node(0);
+var b = new Node(4);
 var c = new Node(3);
-var d = new Node(4);
-var e = new Node(5);
-var f = new Node(6);
-var g = new Node(7);
+var d = new Node(2);
+var e = new Node(0);
+var f = new Node(0);
+var g = new Node(4);
 
 a.next = b;
 b.next = c;
@@ -28,4 +28,63 @@ function printList(head) {
   }
 }
 
-function partitionList(head, partition) {}
+function partitionList(head, partition) {
+  let less = new Node(null);
+  let pointerForLess = less;
+  let greater = new Node(null);
+  let pointerForGreater = greater;
+
+  let node = head;
+
+  while (node) {
+    if (node.val < partition) {
+      if (less.val === null) {
+        less.val = node.val;
+      } else {
+        pointerForLess.next = new Node(node.val);
+        pointerForLess = pointerForLess.next;
+      }
+      node = node.next;
+    } else {
+      if (greater.val === null) {
+        greater.val = node.val;
+      } else {
+        pointerForGreater.next = new Node(node.val);
+        pointerForGreater = pointerForGreater.next;
+      }
+      node = node.next;
+    }
+  }
+
+  if (less.val === null && greater.val === null) return null;
+  if (less.val === null) return greater;
+  if (greater.val === null) return less;
+
+  pointerForLess.next = greater;
+
+  printList(less);
+
+  return less;
+}
+
+function partitionList2(head, partition) {
+  let header = head;
+  let tail = head;
+
+  while (head) {
+    let next = head.next;
+    if (head.val < partition) {
+      head.next = header;
+      header = head;
+    } else {
+      tail.next = head;
+      tail = head;
+    }
+    head = next;
+  }
+  tail.next = null;
+  printList(header);
+  return header;
+}
+
+partitionList2(a, 4);
