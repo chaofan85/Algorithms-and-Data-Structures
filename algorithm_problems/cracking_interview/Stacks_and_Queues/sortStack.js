e1333ef20ddab3fd606569ba4e98378bc7740636
Chao Fan <fanchao05@gmail.com>
2018-09-23 20:48:19 -0400

    updated sortStack

--- algorithm_problems/cracking_interview/Stacks_and_Queues/sortStack.js
+++ algorithm_problems/cracking_interview/Stacks_and_Queues/sortStack.js
@@ -11,7 +11,7 @@ class Stack {
     if (!this.items.length) {
       return "No item to pop";
     }
-    this.items.pop();
+    return this.items.pop();
   }
 
   peek() {
@@ -27,4 +27,33 @@ class Stack {
   }
 }
 
-function sortStack(stack) {}
+function sortStack(stack) {
+  let sorted = new Stack();
+  sorted.push(stack.pop());
+
+  while (!stack.isEmpty()) {
+    if (stack.peek() <= sorted.peek()) {
+      sorted.push(stack.pop());
+    } else {
+      let greater = stack.pop();
+      while (sorted.peek() < greater) {
+        stack.push(sorted.pop());
+      }
+      sorted.push(greater);
+    }
+  }
+
+  return sorted;
+}
+
+let stack = new Stack();
+stack.push(5);
+stack.push(6);
+stack.push(2);
+stack.push(1);
+stack.push(4);
+stack.push(8);
+stack.push(5);
+stack.push(4);
+
+console.log(sortStack(stack));
