0e18cbe1a41005e71e880286851f386094831d8e
Chao Fan <fanchao05@gmail.com>
2018-10-08 17:36:17 -0400

    added max path sum problem

---
+++ algorithm_problems/leetcode/maxPathSum.js
@@ -0,0 +1,21 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var maxPathSum = function(root) {
+  let max = root.val;
+  _maxPath(root);
+  return max;
+
+  function _maxPath(rootNode) {
+    if (!rootNode) return 0;
+    let lsum = _maxPath(rootNode.left);
+    let rsum = _maxPath(rootNode.right);
+    let leftSum = Math.max(0, lsum);
+    let rightSum = Math.max(0, rsum);
+    let thisMax = rootNode.val + leftSum + rightSum;
+    max = Math.max(max, thisMax);
+    return rootNode.val + Math.max(leftSum, rightSum);
+  }
+};
