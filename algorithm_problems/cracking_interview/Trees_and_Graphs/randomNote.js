3a794ab4456fa8f8dee470b6e0d5953356b70c45
Chao Fan <fanchao05@gmail.com>
2018-10-09 22:50:44 -0400

    aded random note problem

---
+++ algorithm_problems/cracking_interview/Trees_and_Graphs/randomNote.js
@@ -0,0 +1,12 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
