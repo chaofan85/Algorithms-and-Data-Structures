555f7e3cc7d51b472565979365aaceb67a804d84
Chao Fan <fanchao05@gmail.com>
2018-10-10 23:37:52 -0400

    updated random note problem

--- algorithm_problems/cracking_interview/Trees_and_Graphs/randomNote.js
+++ algorithm_problems/cracking_interview/Trees_and_Graphs/randomNote.js
@@ -9,4 +9,55 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val, node = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val < node.val) {
+      if (node.left) {
+        this.insert(val, node.left);
+      } else {
+        node.left = new TreeNode(val);
+      }
+    } else {
+      if (node.right) {
+        this.insert(val, node.right);
+      } else {
+        node.right = new TreeNode(val);
+      }
+    }
+  }
+
+  find(val, node = this.root) {
+    if (!node) return;
+
+    let left = this.find(val, node.left);
+    if (left) return left;
+    let right = this.find(val, node.right);
+    if (right) return right;
+    if (node.val === val) return node;
+  }
+
+  delete(val, node = this.root) {}
+
+  inOrder(node = this.root) {
+    if (!node) return;
+    this.inOrder(node.left);
+    console.log(node.val);
+    this.inOrder(node.right);
+  }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(16);
+tree.insert(13);
+tree.insert(4);
+tree.insert(7);
+
+console.log(tree.find(16));
