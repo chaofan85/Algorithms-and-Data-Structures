a6c0dc9aac9d3598279139af861c9f8533b2d88e
Chao Fan <fanchao05@gmail.com>
2018-10-13 02:19:36 -0400

    updated random note problem

--- algorithm_problems/cracking_interview/Trees_and_Graphs/randomNote.js
+++ algorithm_problems/cracking_interview/Trees_and_Graphs/randomNote.js
@@ -8,11 +8,15 @@ class TreeNode {
 class BinarySearchTree {
   constructor() {
     this.root = null;
+    this.size = 0;
+    this.values = [];
   }
 
   insert(val, node = this.root) {
     if (!this.root) {
       this.root = new TreeNode(val);
+      this.size++;
+      this.values.push(val);
       return;
     }
 
@@ -21,28 +25,45 @@ class BinarySearchTree {
         this.insert(val, node.left);
       } else {
         node.left = new TreeNode(val);
+        this.size++;
+        this.values.push(val);
       }
     } else {
       if (node.right) {
         this.insert(val, node.right);
       } else {
         node.right = new TreeNode(val);
+        this.size++;
+        this.values.push(val);
       }
     }
   }
 
-  find(val, node = this.root) {
-    if (!node) return;
-
-    let left = this.find(val, node.left);
-    if (left) return left;
-    let right = this.find(val, node.right);
-    if (right) return right;
-    if (node.val === val) return node;
+  find(val) {
+    let node = this.root;
+    while (node) {
+      if (val < node.val) {
+        node = node.left;
+      } else if (val > node.val) {
+        node = node.right;
+      } else {
+        return node;
+      }
+    }
+    return null;
   }
 
-  delete(val) {
-    let node = this.find(val);
+  // delete(val) {
+  //   let node = this.find(val);
+  // }
+
+  getSize() {
+    return this.size;
+  }
+
+  randomNode() {
+    let randomNum = Math.floor(Math.random() * this.size);
+    return this.find(this.values[randomNum]);
   }
 
   inOrder(node = this.root) {
@@ -62,4 +83,4 @@ tree.insert(13);
 tree.insert(4);
 tree.insert(7);
 
-console.log(tree.find(16));
+console.log(tree.randomNode());
