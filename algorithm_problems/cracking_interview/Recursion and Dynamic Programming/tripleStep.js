9f5ccac2ac0110d8b63c42728cf235c4977e8b0a
Chao Fan <fanchao05@gmail.com>
2018-10-16 19:25:11 -0400

    added triple step problem

---
+++ algorithm_problems/cracking_interview/Recursion and Dynamic Programming/tripleStep.js
@@ -0,0 +1,24 @@
+/*
+ A child is running up a staircase with n steps and can hop either 1 step,
+ 2 steps, or 3 steps at a time. Implement a method to count how many
+ possible ways the child can run up the stairs.
+ */
+
+function tripleStep(num, cache = {}) {
+  if (num <= 0) return 0;
+  if (num === 1) return 1;
+  if (num === 2) return 2;
+  if (num === 3) return 4;
+
+  if (cache[num] !== undefined) return cache[num];
+  let prev1 = tripleStep(num - 1, cache);
+  cache[num - 1] = prev1;
+  let prev2 = tripleStep(num - 2, cache);
+  cache[num - 2] = prev2;
+  let prev3 = tripleStep(num - 3, cache);
+  cache[num - 3] = prev3;
+
+  return prev1 + prev2 + prev3;
+}
+
+console.log(tripleStep(100));
--- review/heap.js
+++ review/heap.js
@@ -1,11 +1,3 @@
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
 class MaxHeap {
   constructor() {
     this.array = [null];
@@ -15,7 +7,7 @@ class MaxHeap {
     this.array.push(val);
     let node = this.array.length - 1;
     let parent = node % 2 === 0 ? node / 2 : (node - 1) / 2;
-    while (this.array[node] > this.arreay[parent] && node !== 1) {
+    while (this.array[node] > this.array[parent] && node !== 1) {
       [this.array[node], this.array[parent]] = [
         this.array[parent],
         this.array[node]
@@ -24,4 +16,71 @@ class MaxHeap {
       parent = node % 2 === 0 ? node / 2 : (node - 1) / 2;
     }
   }
+
+  delete() {
+    [this.array[1], this.array[this.array.length - 1]] = [
+      this.array[this.array.length - 1],
+      this.array[1]
+    ];
+    this.array.pop();
+    this.siftDown(1);
+  }
+
+  siftDown(idx) {
+    if (this.array.length <= 3) {
+      if (this.array[1] < this.array[2]) {
+        [this.array[1], this.array[2]] = [this.array[2], this.array[1]];
+      }
+      return;
+    }
+
+    if (
+      this.array[idx] < this.array[2 * idx] ||
+      this.array[idx] < this.array[2 * idx + 1]
+    ) {
+      if (this.array[2 * idx] < this.array[2 * idx + 1]) {
+        while (
+          this.array[idx] < this.array[2 * idx + 1] &&
+          2 * idx + 1 <= this.array.length - 1
+        ) {
+          [this.array[idx], this.array[2 * idx + 1]] = [
+            this.array[2 * idx + 1],
+            this.array[idx]
+          ];
+          idx = 2 * idx + 1;
+        }
+      } else {
+        while (
+          this.array[idx] < this.array[2 * idx] &&
+          2 * idx <= this.array.length - 1
+        ) {
+          [this.array[idx], this.array[2 * idx]] = [
+            this.array[2 * idx],
+            this.array[idx]
+          ];
+          idx *= 2;
+        }
+      }
+    }
+  }
+
+  print() {
+    console.log(this.array);
+  }
 }
+
+let heap = new MaxHeap();
+
+heap.insert(50);
+heap.insert(75);
+heap.insert(100);
+heap.insert(60);
+heap.insert(45);
+heap.insert(62);
+heap.insert(1);
+
+heap.print();
+
+heap.delete();
+
+heap.print();
