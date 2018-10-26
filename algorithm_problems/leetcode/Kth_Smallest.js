7aeb45bea1bffbfba9fc358ce893ec34cbce045f
Chao Fan <fanchao05@gmail.com>
2018-10-25 23:58:46 -0400

    updated problem

--- algorithm_problems/leetcode/Kth_Smallest.js
+++ algorithm_problems/leetcode/Kth_Smallest.js
@@ -15,16 +15,16 @@
 var kthSmallest = function(root, k) {
   let curr = 0;
 
-  function helper(root, k) {
+  function helper(root) {
     if (!root) return null;
 
-    let left = helper(root.left, k);
+    let left = helper(root.left);
     if (left || left === 0) return left;
 
     curr++;
     if (curr === k) return root.val;
 
-    let right = helper(root.right, k);
+    let right = helper(root.right);
     if (right || right === 0) return right;
   }
 
