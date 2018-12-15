b486a6921a1c0b9f703171836b9a438784569d51
chaofan85 <fanchao05@gmail.com>
2018-12-15 01:57:13 -0500

    finished cheapest price problem

--- algorithm_problems/leetcode/751-800/787. Cheapest_Flights_Within_K_Stops.js
+++ algorithm_problems/leetcode/751-800/787. Cheapest_Flights_Within_K_Stops.js
@@ -4,37 +4,27 @@ var findCheapestPrice = function(n, flights, src, dst, K) {
   let list = getList(flights);
   let minPrice = Infinity;
 
-  function getMinPrice(flightList, start, end, stops, currPrice = 0) {
-    // console.log(flightList[start]["1"]);
-    
+  function getMinPrice(flightList, start, end, stops, currPrice = 0, visited = new Set()) {
+    if (!flightList[start]) return;
     if (stops === 0) {
-      console.log(flightList[start], start, end);
-      
       if (flightList[start][end]) {
         minPrice = Math.min(minPrice, currPrice + flightList[start][end]);
-        
       }
       return;
     }
 
-    // if (start === end) {
-    //   minPrice = Math.min(minPrice, currPrice + flightList[start][end]);
-    // }
-
-    
-
     for (let neighbor in flightList[start]) {
-      console.log(neighbor, end, 'lalal');
-      
+      let price = currPrice + flightList[start][neighbor];
       if (neighbor !== end.toString()) {
-        getMinPrice(flightList, neighbor, end, stops - 1, flightList[start][neighbor]);
-      } else {
-        minPrice = Math.min(minPrice, currPrice + flightList[start][neighbor]);
-      }
+        if (!visited.has(neighbor) && price < minPrice) {
+          visited.add(neighbor);
+          getMinPrice(flightList, neighbor, end, stops - 1, price, visited);
+        }
+      } else minPrice = Math.min(minPrice, price);
+      visited.delete(neighbor);
     }
   }
-  console.log(list);
-  
+
   getMinPrice(list, src, dst, K);
   return minPrice === Infinity ? -1 : minPrice;
 };
@@ -50,8 +40,41 @@ function getList(arr) {
   return list;
 }
 
-// console.log(findCheapestPrice([[0, 1, 100], [1, 2, 100], [0, 2, 500]]));
-
 console.log(
   findCheapestPrice(3, [[0, 1, 100], [1, 2, 100], [0, 2, 300]], 0, 2, 0)
+);
+
+console.log(
+  findCheapestPrice(
+    10,
+    [
+      [3, 4, 4],
+      [2, 5, 6],
+      [4, 7, 10],
+      [9, 6, 5],
+      [7, 4, 4],
+      [6, 2, 10],
+      [6, 8, 6],
+      [7, 9, 4],
+      [1, 5, 4],
+      [1, 0, 4],
+      [9, 7, 3],
+      [7, 0, 5],
+      [6, 5, 8],
+      [1, 7, 6],
+      [4, 0, 9],
+      [5, 9, 1],
+      [8, 7, 3],
+      [1, 2, 6],
+      [4, 1, 5],
+      [5, 2, 4],
+      [1, 9, 1],
+      [7, 8, 10],
+      [0, 4, 2],
+      [7, 2, 8]
+    ],
+    6,
+    0,
+    7
+  )
 );
