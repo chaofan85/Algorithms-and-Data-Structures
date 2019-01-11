7b9d771743359f308433d21e024663cf3a102a91
chaofan85 <fanchao05@gmail.com>
2019-01-10 23:42:54 -0500

    updated problem 406

--- algorithm_problems/leetcode/401-450/406. Queue_Reconstruction_by_Height.js
+++ algorithm_problems/leetcode/401-450/406. Queue_Reconstruction_by_Height.js
@@ -19,5 +19,37 @@ Output:
 */
 
 var reconstructQueue = function(people) {
-  
+  let result = [];
+  people = people.sort((a, b) => {
+    return a[1] < b[1] ? -1 : 1;
+    // return a[0] < b[0] ?  1 : -1;
+  });
+  // console.log(people);
+
+  result.push(people[0]);
+  for (let i = 1; i < people.length; i++) {
+    let j = 0;
+    let counter = 0;
+    while (counter < people[i][1]) {
+      // console.log(i, people[i], counter);
+      // console.log(result)
+      if (people[i][0] <= result[j][0]) counter++;
+      if (j === result.length - 1) break;
+      j++;
+    }
+    while (result[j][1] === people[i][1] && result[j][0] < people[i][0]) {
+      j++;
+      if (j > result.length - 1) break;
+      console.log(i, j);
+    }
+
+    console.log(result, j, people[i], "lalalal");
+    result = result
+      .slice(0, j)
+      .concat([people[i]])
+      .concat(result.slice(j));
+  }
+  return result;
 };
+
+console.log(reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]));
