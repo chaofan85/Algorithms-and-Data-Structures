e5dcab6e47ba1cfbc8e66ec7aa12dba9387a99b8
chaofan85 <fanchao05@gmail.com>
2019-01-12 23:54:42 -0500

    updated solution

--- algorithm_problems/leetcode/001-050/001. Two_Sum.py
+++ algorithm_problems/leetcode/001-050/001. Two_Sum.py
@@ -1,18 +1,18 @@
 def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    
+    hash = {}
+    i = 0
+    while i < len(nums):
+      compli = target - nums[i]
+      if compli in hash:
+        return [min(i, hash[compli]), max(i, hash[compli])]
+      else:
+        hash[nums[i]] = i
+      i += 1
         
-        hash = {}
-        i = 0
-        while i < len(nums):
-          compli = target - nums[i]
-          if compli in hash:
-            return [min(i, hash[compli]), max(i, hash[compli])]
-          else:
-            hash[nums[i]] = i
-          i += 1
-            
-        return []
+    return []
