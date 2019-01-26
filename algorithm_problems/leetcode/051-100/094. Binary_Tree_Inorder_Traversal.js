4f58f51cdcf6936e1aba31b471b35204e43d4e5d
chaofan85 <fanchao05@gmail.com>
2019-01-25 23:28:17 -0500

    added one problem

---
+++ algorithm_problems/leetcode/051-100/094. Binary_Tree_Inorder_Traversal.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+var inorderTraversal = function(root) {
+  let result = [];
+  let stack = [];
+  let temp = root;
+
+  while (temp || stack.length) {
+    while (temp) {
+      stack.push(temp);
+      temp = temp.left;
+    }
+    temp = stack.pop();
+    result.push(temp.val);
+    temp = temp.right;
+  }
+  return result;
+};
